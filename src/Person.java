f411dec2c75daed1d1d1cd90b3811974f936e3fc
Larisa <larisaplantation@gmail.com>
2019-07-20 22:00:58 -0400

    class Person (lecture 17/7/2019)

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,23 @@
 public class Person {
-}
+    String name;
+    int age;
+    String sex;
+/*
+   Person(String name, int age, String sex){
+      this.name = name;
+      this.age = age;
+      this.sex = sex;
+   }
+*/
+   String getName(){
+        if (this.sex.equals("m")){
+            return "Mr." + this.name;
+        } else {
+            return "Ms." + this.name;
+        }
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+ }
