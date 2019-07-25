aff47462d6afcc370123043cf0cd0e54f0e20449
Larisa <larisaplantation@gmail.com>
2019-07-24 21:29:40 -0400

    added text

--- README.md
+++ README.md
@@ -51,7 +51,25 @@ public class Person {
     }
  }
 ```
-
+* [x] Fibonacci sequence
+```java
+public class Main {
+    public static void main(String[] args) {
+      int n = 10;
+      int f0 = 0;
+      int f1 = 1;
+      System.out.println(f0);
+      System.out.println(f1);
+      int f2;
+      for (int i = 2; i <= n; i++) {
+        f2 = f0 + f1;
+        System.out.println(f2);
+        f0 = f1;
+        f1 = f2;
+      }
+    }
+}
+```
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-      Person p1 = new Person();
-      p1.name = "Anna";
-      p1.age = 32;
-      p1.sex = "f";
-
-      Person p2 = new Person();
-      p2.name = "Artem";
-      p2.age = 47;
-      p2.sex = "m";
-
-      System.out.println(p1.name);
-      System.out.println(p2.name);
-
-      System.out.println(p1.getName());
-      System.out.println(p2.getName());
-
-      p1.setName("Amelia");
-      System.out.println(p1.getName());
-      p1.setName("Larisa");
-      System.out.println(p1.getName());
+      int n = 10;
+      int f0 = 0;
+      int f1 = 1;
+      System.out.println(f0);
+      System.out.println(f1);
+      int f2;
+      for (int i = 2; i <= n; i++) {
+        f2 = f0 + f1;
+        System.out.println(f2);
+        f0 = f1;
+        f1 = f2;
+      }
     }
 }
 
