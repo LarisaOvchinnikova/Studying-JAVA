f9a79beade5a67b0cdb2e677d1a3f4db525d3b99
Larisa <larisaplantation@gmail.com>
2019-07-31 23:37:22 -0400

    added text

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
   public static void main(String... args) {
-    Cube c = new Cube();
-    c.setSide(5);
-    System.out.println(c.getSide());
+    Block b = new Block(new int[]{2,4,6});
+    System.out.println(b.getWidth());
+    System.out.println(b.getLength());
+    System.out.println(b.getHeight());
+    System.out.println(b.getVolume());
+    System.out.println(b.getSurfaceArea());
   }
 }
 
